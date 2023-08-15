d8dcf3abf99c4d8a6b96182dcd6d03b4571a64f3
RafaelIrabien <rafa.irabien@gmail.com>
2023-08-15 13:48:58 -0600

    Agregar configuración general del sistema

---
+++ Config/APP.php
@@ -0,0 +1,10 @@
+<?php 
+    //URL donde está alojada toda la aplicación
+    const SERVER_URL = "http://localhost/Prestamos/";
+
+    const COMPANY = "Sistemas Prestamos";
+
+    const MONEDA = "$";
+
+    //Zona horaria
+    date_default_timezone_set("America/Merida");
