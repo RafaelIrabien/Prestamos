ac73a973473cc6e0fe40760bf2e03b87a32ad6b9
RafaelIrabien <rafa.irabien@gmail.com>
2023-08-16 22:49:23 -0600

    Agregar vistas

--- Models/vistasModel.php
+++ Models/vistasModel.php
@@ -6,7 +6,27 @@
         protected static function obtener_vistas_model($vistas) {
             //Lista blanca de palabras que si podrán escribirse en la URL
             //y que si van a obtener vistas mediante esa palabra clave
-            $listaBlanca = ["home","client-list"];
+            $listaBlanca = ["home",
+                            "client-list",
+                            "client-new",
+                            "client-search",
+                            "client-update",
+                            "company",
+                            "item-list",
+                            "item-new",
+                            "item-search",
+                            "item-update",
+                            "reservation-list",
+                            "reservation-new",
+                            "reservation-pending",
+                            "reservation-reservation",
+                            "reservation-search",
+                            "reservation-update",
+                            "user-list",
+                            "user-new",
+                            "user-search",
+                            "user-update"
+                            ];
 
             //Comprobar que una vista esté en nuestro sistema
             if (in_array($vistas, $listaBlanca)) {
