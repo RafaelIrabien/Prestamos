e3e0dd5c975a01277feb984b815eb2fab4571409
RafaelIrabien <rafa.irabien@gmail.com>
2023-08-17 23:16:37 -0600

    Crear función Conectar a la BD

---
+++ Models/mainModel.php
@@ -0,0 +1,21 @@
+<?php
+    //Detectamos si estamos realizando una petición ajax o no
+    if ($peticionAjax) {
+        require_once "../Config/SERVER.php";
+    } else {
+        require_once "./Config/SERVER.php";
+    }
+
+class mainModel {
+
+    /*-------- Función conectar a BD --------*/
+    protected static function conectar() {
+        $conexion = new PDO(SGBD, USER, PASSWORD);
+
+        //La conexión utilizará los caracteres utf8
+        $conexion->exec("SET CHARACTER SET utf8");
+        return $conexion;
+    }
+
+   
+}
