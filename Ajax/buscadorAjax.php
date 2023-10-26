cb63a8148d77ad6473da43f67d178abeb0ed4840
RafaelIrabien <rafa.irabien@gmail.com>
2023-10-26 13:31:44 -0500

    Comprobar que el input 'modulo' venga definido

--- Ajax/buscadorAjax.php
+++ Ajax/buscadorAjax.php
@@ -9,6 +9,33 @@
             "usuario"=>"user-search"
             
         ];
+
+        //Comprobamos que el input "modulo" venga definido
+        if (isset($_POST['modulo'])) {
+            $modulo = $_POST['modulo'];
+            //Comnprobar si el valor está en el array $data_url
+            if (!isset($data_url[$modulo])) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No podemos continuar con la búsqueda debido a un error",
+                    "Tipo"=>"error"
+                   ];
+                echo json_encode($alerta);
+                exit();
+            }
+            
+        } else {
+            $alerta = [
+                "Alerta"=>"simple",
+                "Titulo"=>"Ocurrió un error inesperado",
+                "Texto"=>"No podemos continuar con la búsqueda debido a un error de configuración",
+                "Tipo"=>"error"
+               ];
+            echo json_encode($alerta);
+            exit();
+        }
+        
     } else {
         session_unset();
         session_destroy();
