d34ccd761b1edbd44d1cea76acc8f60354842256
RafaelIrabien <rafa.irabien@gmail.com>
2023-11-02 22:41:35 -0600

    Comprobar que los campos requeridos estén llenos

--- Controllers/clienteController.php
+++ Controllers/clienteController.php
@@ -16,6 +16,20 @@
             $apellido = mainModel::limpiar_cadena($_POST['cliente_apellido_reg']);
             $telefono = mainModel::limpiar_cadena($_POST['cliente_telefono_reg']);
             $direccion = mainModel::limpiar_cadena($_POST['cliente_direccion_reg']);
-        }
+
+            //Comprobar que los campos requeridos estén llenos
+            if ($dni=="" || $nombre=="" || $apellido=="" || $telefono=="" || $direccion=="") {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No has llenado los campos obligatorios",
+                    "Tipo"=>"error"
+                   ];
+                //Pasamos a json el array para que js lo entienda
+                echo json_encode($alerta);
+                exit();
+            }
+
+        } //Finaliza agregar_usuario_controlador()
 
     }
